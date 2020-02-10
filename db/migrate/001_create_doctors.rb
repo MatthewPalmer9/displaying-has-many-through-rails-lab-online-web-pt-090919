9b4a40346bc8189c6bf017830ec025f8f7583ba4
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-10 05:14:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_doctors.rb
+++ db/migrate/001_create_doctors.rb
@@ -4,7 +4,7 @@ class CreateDoctors < ActiveRecord::Migration
       t.string :name
       t.string :department
 
-      t.timestamps :null, false
+      t.timestamps null: false
     end
   end
 end
